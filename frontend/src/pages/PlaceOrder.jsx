8c6d54f1cf532e881eddc0e5dfb312bd7263a145
jaskirat-singh-12 <jaskirat-singh-12>
2025-06-17 18:30:11 +0530

    no stripe and rasorpay

--- frontend/src/pages/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder.jsx
@@ -152,15 +152,15 @@ const PlaceOrder = () => {
                         <Title text1={'PAYMENT'} text2={'METHOD'} />
                         {/* --------------- Payment Method Selection ------------- */}
                         <div className='flex gap-3 flex-col lg:flex-row'>
-                            <div onClick={() => method('stripe')} className='flex items-center gap-3 border p-2 px-3 cursor-pointer'>
+                            {/* <div onClick={() => method('stripe')} className='flex items-center gap-3 border p-2 px-3 cursor-pointer'>
                                 <p className={`min-w-3.5 h-3.5 border rounded-full ${method === 'stripe' ? 'bg-green-400' : ''}`}></p>
                                 <img className='h-5 mx-4' src={assets.stripe_logo} alt="Stripe" />
-                            </div>
+                            </div> */}
 
-                            <div onClick={() => method('razorpay')} className='flex items-center gap-3 border p-2 px-3 cursor-pointer'>
+                            {/* <div onClick={() => method('razorpay')} className='flex items-center gap-3 border p-2 px-3 cursor-pointer'>
                                 <p className={`min-w-3.5 h-3.5 border rounded-full ${method === 'razorpay' ? 'bg-green-400' : ''}`}></p>
                                 <img className='h-5 mx-4' src={assets.razorpay_logo} alt="Razorpay" />
-                            </div>
+                            </div> */}
 
                             <div onClick={() => method('cod')} className='flex items-center gap-3 border p-2 px-3 cursor-pointer'>
                                 <p className={`min-w-3.5 h-3.5 border rounded-full ${method === 'cod' ? 'bg-green-400' : ''}`}></p>
